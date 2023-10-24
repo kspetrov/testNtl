0c14dc0038c6342705cd3731306f1791246cb2e8
kspetrov <kspetrov@gmail.com>
2023-10-24 23:55:50 +0300

    q

--- src/auth/netlifyAuth.js
+++ src/auth/netlifyAuth.js
@@ -1,40 +1,27 @@
-import netlifyIdentity from 'netlify-identity-widget';
+import netlifyIdentity from "netlify-identity-widget";
 
 const netlifyAuth = {
   initialize(callback) {
     window.netlifyIdentity = netlifyIdentity;
-    netlifyIdentity.on('init', async user => {
+    netlifyIdentity.on("init", async (user) => {
       if (!user) {
         callback(user);
         return;
       }
-
-      // refresh token if needed
-      await this.refresh(callback);
     });
     netlifyIdentity.init();
   },
-  async refresh(callback) {
-    // refresh token if needed
-    try {
-      await netlifyIdentity.refresh(true);
-      callback(netlifyIdentity.currentUser());
-    } catch (error) {
-      netlifyIdentity.logout();
-      callback(null);
-    }
-  },
   authenticate(callback, onClose) {
     netlifyIdentity.open();
-    netlifyIdentity.on('login', user => {
+    netlifyIdentity.on("login", (user) => {
       callback(user);
       netlifyIdentity.close();
     });
-    netlifyIdentity.on('close', onClose);
+    netlifyIdentity.on("close", onClose);
   },
   signout(callback) {
     netlifyIdentity.logout();
-    netlifyIdentity.on('logout', () => {
+    netlifyIdentity.on("logout", () => {
       callback();
     });
   },
